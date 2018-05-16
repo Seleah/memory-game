638b6c3d668d063de4302da0eb1ec705c26441ed
Seleah OConnor <seleah.face@gmail.com>
2018-05-16 20:18:55 +0000

    Put code to flip card into function displaySymbol

--- js/app.js
+++ js/app.js
@@ -47,27 +47,47 @@ displayRandomCards(cardsList);
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Set up the event listener for a card. If a card is clicked:
+ *
+ *  - Display the card's symbol (put this functionality in another function that 
+ *    you call from this one) 
+  --------------------------------------Done----------------------------------
+ *    
+ *  - Add the card to a *list* of "open" cards (put this functionality in another
+ *    function that you call from this one)
+ *    
+ *  - If the list already has another card, check to see if the two cards match
+ *
+ *    + If the cards do match, lock the cards in the open position (put this 
+ *      functionality in another function that you call from this one)
+ *      
+ *    + If the cards do not match, remove the cards from the list and hide the 
+ *      card's symbol (put this functionality in another function that you call 
+ *      from this one)
+ *      
+ *    + Increment the move counter and display it on the page (put this 
+ *      functionality in another function that you call from this one)
+ *      
+ *    + If all cards have matched, display a message with the final score 
+ *      (put this functionality in another function that you call from this one)
  */
 
 let deck = document.body.querySelector('.deck');
 
-deck.addEventListener('click', function(e) {
+function displaySymbol(elementClicked) {
     // ensure proper flipping of entire card, but not just a side
-    if (e.target != deck) {
-        if (e.target.classList.contains('fa')) {
-            let side = e.target.parentNode;
+    if (elementClicked != deck) {
+        if (elementClicked.classList.contains('fa')) {
+            let side = elementClicked.parentNode;
             side.parentNode.classList.toggle('flipped');
         } else {
-            let side = e.target;
+            let side = elementClicked;
             side.parentNode.classList.toggle('flipped');
         }
     }
+}
+
+deck.addEventListener('click', function(e) {
+    
+    displaySymbol(e.target);
 });
